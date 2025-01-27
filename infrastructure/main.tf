ea2f044f940224310e88ef630f1fe012e5a784b2
Suraj <suraj.ukumar.p@gmail.com>
2025-01-27 23:50:29 +0530

    feat: add IaC with Terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,60 +1,71 @@
-# Create a VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+  
 }
 
-# create a subnet 1
 resource "aws_subnet" "subnet_1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.0/20"
-  availability_zone = "us-east-1b"
-  map_public_ip_on_launch = true
-}
+    vpc_id     = aws_vpc.main.id
+    cidr_block = "10.0.0.0/20"
+    availability_zone = "ap-south-1a"
+    map_public_ip_on_launch = true
+    }
 
-# create a subnet 2
 resource "aws_subnet" "subnet_2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.16.0/20"
-  availability_zone = "us-east-1c"
-  map_public_ip_on_launch = true
-}
+    vpc_id     = aws_vpc.main.id
+    cidr_block = "10.0.16.0/20"
+    availability_zone = "ap-south-1b"
+    map_public_ip_on_launch = true
+    }
 
-# create a subnet 3
 resource "aws_subnet" "subnet_3" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.32.0/20"
-  availability_zone = "us-east-1d"
-  map_public_ip_on_launch = true
-}
-
+    vpc_id     = aws_vpc.main.id
+    cidr_block = "10.0.32.0/20"
+    availability_zone = "ap-south-1c"
+    map_public_ip_on_launch = true
+    }
 
 resource "aws_internet_gateway" "internet_gw" {
-    vpc_id = aws_vpc.main.id  
+    vpc_id = aws_vpc.main.id
 }
 
 resource "aws_route_table" "route_table" {
-    vpc_id =  aws_vpc.main.id
-
+    vpc_id = aws_vpc.main.id
 
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.internet_gw.id
+
     }
 
     route {
         cidr_block = "10.0.0.0/16"
         gateway_id = "local"
-    }
+        }
 }
 
-# Use of EKS Module
+
+resource "aws_route_table_association" "subnet_1_association" {
+    subnet_id      = aws_subnet.subnet_1.id
+    route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet_2_association" {
+    subnet_id      = aws_subnet.subnet_2.id
+    route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet_3_association" {
+    subnet_id      = aws_subnet.subnet_3.id
+    route_table_id = aws_route_table.route_table.id
+}
+
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = "devops-url2qr-capstone"
+  cluster_name    = "devops-capstone-project"
   cluster_version = "1.27"
 
   cluster_endpoint_public_access = true
@@ -66,7 +77,7 @@ module "eks" {
   eks_managed_node_groups = {
     green = {
       min_size       = 1
-      max_size       = 1
+      max_size       = 2
       desired_size   = 1
       instance_types = ["t3.medium"]
     }
--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -7,7 +7,9 @@ terraform {
   }
 }
 
+
+
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
