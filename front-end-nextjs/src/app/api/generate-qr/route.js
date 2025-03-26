b9092f8d5b3df45442492a27a072ce0467f65a3a
Suraj <suraj.ukumar.p@gmail.com>
2025-03-26 19:06:42 +0530

    fix: deployment fixes

--- front-end-nextjs/src/app/api/generate-qr/route.js
+++ front-end-nextjs/src/app/api/generate-qr/route.js
@@ -1,13 +1,6 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
-// Determine the API base URL dynamically
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ||
-  (process.env.NODE_ENV === "production"
-    ? "http://qr-backend-service" // Kubernetes Service
-    : "http://backend:8000"); // Docker Compose
-
 export async function POST(request) {
   const { searchParams } = new URL(request.url);
   const url = searchParams.get("url");
@@ -18,7 +11,7 @@ export async function POST(request) {
 
   try {
     const response = await axios.post(
-      `${API_BASE_URL}/generate-qr/?url=${encodeURIComponent(url)}`
+      `http://qr-backend-service/generate-qr/?url=${encodeURIComponent(url)}`
     );
     return NextResponse.json(response.data);
   } catch (error) {
